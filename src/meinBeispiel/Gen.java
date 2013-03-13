5b6000e1cf254389eb030faa2e4c51b1581fa26a
malcjohn <cincenko@seznam.cz>
2013-03-13 09:15:57 +0100

    upa

---
+++ src/meinBeispiel/Gen.java
@@ -0,0 +1,21 @@
+package meinBeis;
+
+public class Gen<T> {
+	public T name;
+
+	public Gen(T name) {
+		this.name = name;
+	}
+
+	public T name() {
+		return name;
+	}
+	public void name(T name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return (String) name;
+	}
+}
---
+++ src/meinBeispiel/Tets.java
@@ -0,0 +1,12 @@
+package meinBeis;
+
+public class Tets {
+
+	public static void main(String[] args) {
+		Gen<String> a = new Gen<String>("sdfdsf");
+		
+		System.out.println(a.toString());
+		
+	}
+
+}
