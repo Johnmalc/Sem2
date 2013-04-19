d48f1e67e673121bd3c3b751e75cb0b79a023ea1
malcjohn <cincenko@seznam.cz>
2013-04-20 00:08:47 +0200

    fdbgv

--- src/msfp06/Ampleschalter.java
+++ src/msfp06/Ampleschalter.java
@@ -3,16 +3,16 @@ package msfp06;
 public class Ampleschalter {	
 	
 	public enum Schalter {	
-		RED(1), GRUN(2);
+		RED, GRUN;
 		
-		private int a;
-		
-		private Schalter(int A){
-			this.a = A;
-		}
-		public int getC(){
-			return a;
-		}
+//		private int a;
+//		
+//		private Schalter(int A){
+//			this.a = A;
+//		}
+//		public int getC(){
+//			return a;
+//		}
 
 	}
 }
--- src/msfp06/Main.java
+++ src/msfp06/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		Klasse kl = new Klasse();
-		kl.druckeSchal();
+		for (int i = 0; i < 3; i++) {
+			kl.druckeSchal();
+		}
 	}
 
 }
